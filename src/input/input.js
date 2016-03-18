9782ec955cda995dcfdccf7c5122f2aa054ed7f5
Sandeep Nambiar <sandeep.turing@gmail.com>
2016-03-18 18:21:53 +0530

    Add basic input scaffolding

---
+++ src/input/input.js
@@ -0,0 +1,20 @@
+var Vector2 = require('three').Vector2;
+var Signal = require('../core/signal.js');
+
+var Input = function(game) {
+  this.game = game;
+};
+
+Input.prototype = {
+  init: function() {
+
+  },
+
+  update: function() {
+
+  }
+};
+
+Input.prototype.constructor = Input;
+
+module.exports = Input;
